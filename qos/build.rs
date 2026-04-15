ba2dc81c4be6db66148f6ee402bc3f2c1d1acad3
Le <travis.baker24.th@gmail.com>
2026-04-15 11:25:26 +0800

    Fix build issue

--- qos/build.rs
+++ qos/build.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let package = aya_build::Package {
-        name: "qos-ebpf",
-        root_dir: concat!(env!("CARGO_MANIFEST_DIR"), "/../qos-ebpf"),
-        no_default_features: false,
-        features: &[],
-    };
-    aya_build::build_ebpf([package], aya_build::Toolchain::Nightly)
-        .expect("failed to build eBPF program");
+    aya_build::build_ebpf(
+        [aya_build::Package {
+            name: "qos-ebpf",
+            // Let aya-build resolve the path from the workspace.
+            // Using CARGO_MANIFEST_DIR ensures an absolute path.
+            root_dir: concat!(env!("CARGO_MANIFEST_DIR"), "/../qos-ebpf"),
+            no_default_features: false,
+            features: &[],
+        }],
+        aya_build::Toolchain::Nightly,
+    )
+    .expect("failed to build eBPF program");
 }
